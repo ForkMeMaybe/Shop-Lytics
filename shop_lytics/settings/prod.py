e6831b13a2a7c2bc1e217021719b37bece201391
fork_me_maybe <forkmemaybe@gmail.com>
2025-09-15 22:51:28 +0530

    changes prod.py

--- shop_lytics/settings/prod.py
+++ shop_lytics/settings/prod.py
@@ -1,6 +1,9 @@
 from .common import *
 import dj_database_url
 import os
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
 
@@ -35,8 +38,6 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-EMAIL_PORT = 465
-EMAIL_USE_SSL = True
-EMAIL_USE_TLS = False
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
